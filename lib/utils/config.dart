25865fc12900e4f59b07902e2a6e55149cac33f0
Shashi <digivisibility@gmail.com>
2026-03-10 18:28:46 +0530

    Initial commit

---
+++ lib/utils/config.dart
@@ -0,0 +1,13 @@
+// ignore: non_constant_identifier_names
+String get OnesignalID => '40a574c6-b53f-427fjifj-fsjfksfkshfs';
+String contactsupportEmail = "support@gmail.com";
+String imgBaseurl = "https://astro.diploy.in/";
+String pdfBaseurl = "https://astro.diploy.in/public";
+String termsconditionurl = "https://astro.diploy.in/terms-and-condition";
+String privacyUrl = "https://astro.diploy.in/privacy-and-policy";
+String appMode = "DEV";
+String storiesIcon = "assets/images/stories_icon.png";
+Map<String, dynamic> appParameters = {
+  "LIVE": {"apiUrl": "https://astro.diploy.in/api/"},
+  "DEV": {"apiUrl": "https://astro.diploy.in/api/"},
+};
